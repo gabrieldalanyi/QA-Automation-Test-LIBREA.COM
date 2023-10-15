a81a4fcdb0bfa3a6a023d38ada2dd19d55535f1c
Gabriel Dalanyi <50451712+gabrieldalanyi@users.noreply.github.com>
2023-10-15 19:15:51 +0300

    Add files via upload

---
+++ src/utils/Constans.java
@@ -0,0 +1,18 @@
+package utils;
+
+public class Constans {
+
+        public static final String USER_EMAIl = "minebostudio@gmail.com";
+        public static final String USER_PASS = "Test1234";
+
+        public static final String INVALID_USER_EMAIl = "test@gmail.com";
+        public static final String INVALID_USER_PASS = "Test1234";
+
+        public static final String NEW_USER_PASS = "1234Test";
+
+
+
+        public static final String NEW_CONFIRM_USER_PASS = "";
+    }
+
+
